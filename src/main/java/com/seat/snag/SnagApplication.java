3e07fd32929ebf873f87dcd982e15735dc53a2d8
Dominic Wood <dominic.wood@theapsgroup.com>
2026-08-28 21:37:35 +0100

    fix: app directory namings

--- src/main/java/com/seat/snag/SnagApplication.java
+++ src/main/java/com/seat/snag/SnagApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.seat.snag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SnagApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SnagApplication.class, args);
 	}
 
 }
